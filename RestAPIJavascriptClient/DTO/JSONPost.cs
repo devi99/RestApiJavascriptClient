b5759136e894003e4b25340be57504b09fd36ad1
Lennert Dael <lennert.dael@live.com>
2018-09-16 23:35:14 +0200

    v1

---
+++ RestAPIJavascriptClient/DTO/JSONPost.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RestAPIJavascriptClient.DTO
+{
+    public class JSONPost
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ RestAPIJavascriptClient/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace RestAPIJavascriptClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Configure CORS so the API allows requests from JavaScript.  
+            // For demo purposes, all origins/headers/methods are allowed.  
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOriginsHeadersAndMethods",
+                    builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors("AllowAllOriginsHeadersAndMethods");
+            app.UseMvc();
+        }
+    }
+}
